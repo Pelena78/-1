c071e78155524bbfddf5bf7c0370d8c21abc85ea
Pelena78 <90238210+Pelena78@users.noreply.github.com>
2021-09-19 23:40:14 +0300

    Delete Создание таблицы users.sql

--- Создание таблицы users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
